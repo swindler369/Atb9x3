cc83d702721646c69c44b02c54516d453749867c
swindler369 <sharansgs007@gmail.com>
2024-11-03 13:38:14 +0530

    stg

---
+++ src/Oct/ex_02102024/Lab001.java
@@ -0,0 +1,7 @@
+package Oct.ex_02102024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println( "Hello Worldss");
+    }
+}
---
+++ src/Task/task031124/Task1.java
@@ -0,0 +1,9 @@
+package Task.task031124;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("10*1=10");
+        System.out.println("10*1=20");
+        System.out.println("10*1=30");
+    }
+}
